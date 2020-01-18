1cb869f86845f32d5bbbf96f7faf4fdba7124842
technologic808 <venkys321@gmail.com>
2020-01-18 15:12:35 +0530

    Add cart service, cart and shipping components

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -10,8 +12,15 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 product;
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
+
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
 ngOnInit() {
